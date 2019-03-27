c6f5fd3f12e4071fffcd55f3ee79b5471dba9239
Aliaksei Karneyeu <aliaksei.karneyeu@grandparade.co.uk>
2019-03-27 18:37:47 +0100

    analyze hugo pages properly

--- main.go
+++ main.go
@@ -2,54 +2,50 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	// "errors"
+	"flag"
 	"fmt"
 	"os"
 	"regexp"
 	"sort"
+	"strings"
 	"time"
 )
 
-type LogLine struct {
-	remote_addr string
-	remote_user string
-	time_local  string
-	// time_local      time.Time
-	request         string
-	status          string
-	body_bytes_sent string
-	http_referer    string
-	http_user_agent string
-}
-
-func ConvertLogLine(logLine string) LogLine {
-	re := regexp.MustCompile(`(?P<remote_addr>\d+\.\d+\.\d+\.\d+) - - \[(?P<time_local>[^\]]+)\] \"(?P<request>.*)\" (?P<status>[0-9]+) (?P<body_bytes_sent>[0-9]+) \"(?P<http_referer>.*)\" \"(?P<http_user_agent>.+)\"`)
-
-	m := reSubMatchMap(re, logLine)
-
-	parsedLine := LogLine{
-		remote_addr:     m["remote_addr"],
-		remote_user:     m["remote_user"],
-		time_local:      m["time_local"],
-		request:         m["request"],
-		status:          m["status"],
-		body_bytes_sent: m["body_bytes_sent"],
-		http_referer:    m["http_referer"],
-		http_user_agent: m["http_user_agent"],
+func ConvertLogLineToMap(logLine string) map[string]string {
+	regexPattern := `(?P<remote_addr>\d+\.\d+\.\d+\.\d+) - -` +
+		` \[(?P<time_local>[^\]]+)\] \"(?P<request>.*)\" (?P<status>[0-9]+)` +
+		` (?P<body_bytes_sent>[0-9]+) \"(?P<http_referer>.*)\" \"` +
+		`(?P<http_user_agent>.+)\"`
+	re := regexp.MustCompile(regexPattern)
+	parsedMap := make(map[string]string)
+	match := re.FindStringSubmatch(logLine)
+	if match == nil {
+		return nil
 	}
-	return parsedLine
-}
-
-func reSubMatchMap(r *regexp.Regexp, str string) map[string]string {
-	match := r.FindStringSubmatch(str)
-	subMatchMap := make(map[string]string)
-	for i, name := range r.SubexpNames() {
+	for i, name := range re.SubexpNames() {
 		if i != 0 {
-			subMatchMap[name] = match[i]
+			parsedMap[name] = match[i]
 		}
 	}
-	return subMatchMap
+	return parsedMap
+}
+
+func matchAllRequirements(parsedLine map[string]string, period string) bool {
+	request := regexp.MustCompile(`GET (.+\.html|\/\d*\/\d*\/\d*\/.*\/)(\?.*)? HTTP\/1\.[10]`)
+	http_user_agent := regexp.MustCompile(`.*([Bb]ot|vkShare|Google-AMPHTML|feedly|[cC]rawler|[Pp]arser|curl|-|Disqus).*`)
+	switch {
+	case parsedLine["status"] != "200":
+		return false
+	case !request.MatchString(parsedLine["request"]):
+		return false
+	case http_user_agent.MatchString(parsedLine["http_user_agent"]):
+		return false
+	case !dateIsInInterval(parsedLine["time_local"], period):
+		return false
+	default:
+		return true
+	}
 }
 
 func sortByPopularity(metric map[string]int) {
@@ -67,7 +63,23 @@ func sortByPopularity(metric map[string]int) {
 			fmt.Printf("%d - %s\n", k, s)
 		}
 	}
+}
 
+func AnonymizeIp(ip string) string {
+	return ip[:strings.LastIndex(ip, ".")+1] + "0"
+}
+
+func ConvertMapToLogLine(parsedLine map[string]string) string {
+	remote_addr := parsedLine["remote_addr"]
+	time_local := parsedLine["time_local"]
+	request := parsedLine["request"]
+	status := parsedLine["status"]
+	body_bytes_sent := parsedLine["body_bytes_sent"]
+	http_referer := parsedLine["http_referer"]
+	http_user_agent := parsedLine["http_user_agent"]
+	return fmt.Sprintf(`%s - - [%s] "%s" %s %s "%s" "%s"`, remote_addr,
+		time_local, request, status, body_bytes_sent, http_referer,
+		http_user_agent)
 }
 
 func dateIsInInterval(line string, period string) bool {
@@ -85,16 +97,7 @@ func dateIsInInterval(line string, period string) bool {
 	}
 	t, _ := time.Parse("02/Jan/2006:15:04:05 -0700", line)
 	return startDate.Before(t)
-
 }
-
-func LogLineString(logLine LogLine) string {
-	return fmt.Sprintf(`%s - - [%s] "%s" %s %s "%s" "%s"`, logLine.remote_addr, logLine.time_local, logLine.request, logLine.status, logLine.body_bytes_sent, logLine.http_referer, logLine.http_user_agent)
-}
-
-// func filter (str string, re *regexp) bool {
-
-// }
 
 func main() {
 
@@ -106,55 +109,53 @@ func main() {
 
 	}
 
-	i := 0
+	// i := 0
 
 	scanner := bufio.NewScanner(os.Stdin)
-	userAgents := make(map[string]int)
-	referers := make(map[string]int)
-	requests := make(map[string]int)
-	statuses := make(map[string]int)
-	logLines := make([]LogLine, 10000)
+	// userAgents := make(map[string]int)
+	// referers := make(map[string]int)
+	// requests := make(map[string]int)
+	// statuses := make(map[string]int)
+	// logLines := make([]LogLine, 10000)
 
 	for scanner.Scan() {
 		// `Text` returns the current token, here the next line,
 		// from the input.
 
 		// fmt.Println(scanner.Text())
-		parsedLine := ConvertLogLine(scanner.Text())
-		i += 1
+		// fmt.Println(scanner.Text())
+		parsedLine := ConvertLogLineToMap(scanner.Text())
+		// i += 1
 		// Write out the uppercased line.
-		logLines = append(logLines, parsedLine)
+		// logLines = append(logLines, parsedLine)
 		// if i > 40 {
 		// 	break
 		// }
 
-	}
-	for line := range logLines {
-		match, _ := regexp.MatchString(`GET .+\.html HTTP\/1\.[1]`, logLines[line].request)
-		if match != true {
-			continue
-		}
-		match, _ = regexp.MatchString(`.*([Bb]ot|vkShare|Google-AMPHTML|feedly|[cC]rawler|[Pp]arser|curl|-).*`, logLines[line].http_user_agent)
-		if match == true {
-			continue
-		}
-		if !dateIsInInterval(logLines[line].time_local, *period) {
-			continue
-		}
+		// }
+		// for line := range logLines {
 
-		userAgents[logLines[line].http_user_agent] += 1
-		referers[logLines[line].http_referer] += 1
-		requests[logLines[line].request] += 1
-		statuses[logLines[line].status] += 1
-		fmt.Println(LogLineString(logLines[line]))
+		if matchAllRequirements(parsedLine, *period) == false {
+			continue
+		}
+		parsedLine["remote_addr"] = AnonymizeIp(parsedLine["remote_addr"])
+		fmt.Println(ConvertMapToLogLine(parsedLine))
+		// userAgents[parsedLine["http_user_agent"]] += 1
+		// referers[parsedLine["http_referer"]] += 1
+		// 	requests[logLines[line].request] += 1
+		// 	statuses[logLines[line].status] += 1
+		// 	fmt.Println(LogLineString(logLines[line]))
 	}
-	// sortByPopularity(userAgents)
+	// sortByPopularity(referers)
 
-	// Check for errors during `Scan`. End of file is
-	// expected and not reported by `Scan` as an error.
+	// // Check for errors during `Scan`. End of file is
+	// // expected and not reported by `Scan` as an error.
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
+	// logLine := `52.87.65.11 - - [02/Nov/2018:06:55:13 +0000] "GET /feed.xml HTTP/1.1" 200 9810 "-" "curl"`
+	// parsedLine := ConvertLogLineToMap(logLine)
+	// fmt.Println(parsedLine)
 
 }
--- main_test.go
+++ main_test.go
@@ -1,72 +1,151 @@
 package main
 
 import (
-	// "fmt"
 	"testing"
 )
 
-// type logLine struct {
-// 	remote_addr string
-// 	remote_user string
-// 	time_local  string
-// 	// time_local      time.Time
-// 	request         string
-// 	status          int
-// 	body_bytes_sent int
-// 	http_referer    string
-// 	http_user_agent string
-// }
+func assertMaps(t *testing.T, got, want map[string]string) {
+	t.Helper()
+	for k, _ := range want {
+		if got[k] != want[k] {
+			t.Errorf("\nexpected value: %s\n     got value: %s", want[k], got[k])
+		}
+	}
+
+}
 
 func TestConvertLogLine(t *testing.T) {
-	logLine := `52.87.65.11 - - [02/Nov/2018:06:55:13 +0000] "GET /feed.xml HTTP/1.1" 200 9810 "-" "curl"`
-	parsedLine := ConvertLogLine(logLine)
-	parsedStructure := LogLine{
-		remote_addr:     "52.87.65.11",
-		remote_user:     "",
-		time_local:      "02/Nov/2018:06:55:13 +0000",
-		request:         "GET /feed.xml HTTP/1.1",
-		status:          "200",
-		body_bytes_sent: "9810",
-		http_referer:    "-",
-		http_user_agent: "curl",
-	}
+	logLine := ""
+	t.Run("Parsing normal log entry", func(t *testing.T) {
+		logLine = `52.87.65.11 - - [02/Nov/2018:06:55:13 +0000] "GET /feed.xml HTTP/1.1" 200 9810 "-" "curl"`
+		expectedResult := map[string]string{
+			"remote_addr":     "52.87.65.11",
+			"remote_user":     "",
+			"time_local":      "02/Nov/2018:06:55:13 +0000",
+			"request":         "GET /feed.xml HTTP/1.1",
+			"status":          "200",
+			"body_bytes_sent": "9810",
+			"http_referer":    "-",
+			"http_user_agent": "curl",
+		}
+		parsedLine := ConvertLogLineToMap(logLine)
+		assertMaps(t, parsedLine, expectedResult)
+	})
 
-	if parsedLine != parsedStructure {
-		t.Errorf("expected:\n%v\n got: %v", parsedStructure, parsedLine)
-	}
-	logLine = `190.128.131.6 - - [03/Nov/2018:23:25:08 +0000] "" 400 0 "-" "-"`
-	parsedLine = ConvertLogLine(logLine)
-	parsedStructure = LogLine{
-		remote_addr:     "190.128.131.6",
-		remote_user:     "",
-		time_local:      "03/Nov/2018:23:25:08 +0000",
-		request:         "",
-		status:          "400",
-		body_bytes_sent: "0",
-		http_referer:    "-",
-		http_user_agent: "-",
-	}
+	t.Run("Parsing normal log entry with empty user agent", func(t *testing.T) {
+		logLine = `190.128.131.6 - - [03/Nov/2018:23:25:08 +0000] "" 400 0 "-" "-"`
+		expectedResult := map[string]string{
+			"remote_addr":     "190.128.131.6",
+			"remote_user":     "",
+			"time_local":      "03/Nov/2018:23:25:08 +0000",
+			"request":         "",
+			"status":          "400",
+			"body_bytes_sent": "0",
+			"http_referer":    "-",
+			"http_user_agent": "-",
+		}
+		parsedLine := ConvertLogLineToMap(logLine)
+		assertMaps(t, parsedLine, expectedResult)
+	})
 
-	if parsedLine != parsedStructure {
-		t.Errorf("expected:\n%v\n got: %v", parsedStructure, parsedLine)
-	}
-	// fmt.Println(parsedLine)
+	t.Run("Parsing strange line that should fail", func(t *testing.T) {
+		logLine = `190.XXXX.XXXXX.XXX - - [03/Nov/2018:23:25:08 +0000] "" 400 0 "-" "-"`
+		parsedLine := ConvertLogLineToMap(logLine)
+		assertMaps(t, parsedLine, nil)
+	})
 }
-func TestLogLineString(t *testing.T) {
-	logLine := `52.87.65.11 - - [02/Nov/2018:06:55:13 +0000] "GET /feed.xml HTTP/1.1" 200 9810 "-" "curl"`
-	parsedStructure := LogLine{
-		remote_addr:     "52.87.65.11",
-		remote_user:     "",
-		time_local:      "02/Nov/2018:06:55:13 +0000",
-		request:         "GET /feed.xml HTTP/1.1",
-		status:          "200",
-		body_bytes_sent: "9810",
-		http_referer:    "-",
-		http_user_agent: "curl",
-	}
-	outputLine := LogLineString(parsedStructure)
-	if outputLine != logLine {
-		t.Errorf("expected:\n%v\n got:\n%v", logLine, outputLine)
-	}
 
+func TestAnonymizeIp(t *testing.T) {
+	cases := map[string]string{
+		"5.255.250.183":   "5.255.250.0",
+		"141.8.144.9":     "141.8.144.0",
+		"178.154.244.157": "178.154.244.0",
+		"40.77.167.89":    "40.77.167.0",
+		"66.249.69.70":    "66.249.69.0",
+	}
+	for testCase, expectedResult := range cases {
+		actualResult := AnonymizeIp(testCase)
+		if actualResult != expectedResult {
+			t.Errorf("result for line: '%s'\n returned wrong value: %s, expected: %s", testCase, actualResult, expectedResult)
+		}
+	}
 }
+
+func TestLogLineOK(t *testing.T) {
+	cases := map[string]bool{
+		`159.203.112.40 - - [05/Jan/2019:22:35:12 +0000] "GET / HTTP/1.0" 200 13444 "-" "Mozilla/5.0 (compatible; NetcraftSurveyAgent/1.0; +info@netcraft.com)"`:                                                                          true,
+		`213.138.93.47 - - [05/Jan/2019:23:36:41 +0000] "GET /2018/08/25/aws-certification-preparation.html HTTP/1.1" 200 7779 "https://www.google.com/" "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0"`: true,
+		`213.138.93.47 - - [05/Jan/2019:23:36:42 +0000] "GET / HTTP/1.1" 200 4402 "https://makvaz.com/2018/08/25/aws-certification-preparation.html" "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0"`:     true,
+		`199.16.157.180 - - [02/Jan/2019:11:54:10 +0000] "GET /2018/09/25/effective-devops.html HTTP/1.1" 200 10399 "-" "Twitterbot/1.0"`:                                                                                                 false,
+		`199.16.157.183 - - [02/Jan/2019:11:54:10 +0000] "GET /assets/img/header-pic.jpeg HTTP/1.1" 200 696591 "-" "Twitterbot/1.0"`:                                                                                                      false,
+		`54.36.148.130 - - [02/Jan/2019:12:06:17 +0000] "GET /blog/page3/ HTTP/1.1" 200 3586 "-" "Mozilla/5.0 (compatible; AhrefsBot/6.1; +http://ahrefs.com/robot/)"`:                                                                    false,
+		`125.212.217.215 - - [02/Jan/2019:16:03:08 +0000] "GET /robots.txt HTTP/1.1" 200 40 "-" "-"`:                                                                                                                                      false,
+		`40.77.167.146 - - [02/Jan/2019:16:03:59 +0000] "GET /about/ HTTP/1.1" 200 3193 "-" "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)"`:                                                                    false,
+	}
+	for testCase, expectedResult := range cases {
+		parsedLine := ConvertLogLineToMap(testCase)
+		actualResult := matchAllRequirements(parsedLine)
+		if actualResult != expectedResult {
+			t.Errorf("result for line: '%s'\n returned wrong value: %t, expected: %t", testCase, actualResult, expectedResult)
+
+		}
+	}
+}
+
+func TestConvertMapToLogLine(t *testing.T) {
+	// logLine := ""
+	t.Run("Parsing normal map entry", func(t *testing.T) {
+		expectedLogLine := `52.87.65.11 - - [02/Nov/2018:06:55:13 +0000] "GET /feed.xml HTTP/1.1" 200 9810 "-" "curl"`
+		parsedMap := map[string]string{
+			"remote_addr":     "52.87.65.11",
+			"remote_user":     "",
+			"time_local":      "02/Nov/2018:06:55:13 +0000",
+			"request":         "GET /feed.xml HTTP/1.1",
+			"status":          "200",
+			"body_bytes_sent": "9810",
+			"http_referer":    "-",
+			"http_user_agent": "curl",
+		}
+		generatedLogLine := ConvertMapToLogLine(parsedMap)
+		if expectedLogLine != generatedLogLine {
+			t.Errorf("Log line did not match!.\nExpected: '%s'\n      Got:'%s'", expectedLogLine, generatedLogLine)
+		}
+	})
+	t.Run("Parsing normal map entry with anonimized ip", func(t *testing.T) {
+		expectedLogLine := `190.128.131.0 - - [03/Nov/2018:23:25:08 +0000] "" 400 0 "-" "-"`
+		parsedMap := map[string]string{
+			"remote_addr":     "190.128.131.6",
+			"remote_user":     "",
+			"time_local":      "03/Nov/2018:23:25:08 +0000",
+			"request":         "",
+			"status":          "400",
+			"body_bytes_sent": "0",
+			"http_referer":    "-",
+			"http_user_agent": "-",
+		}
+		parsedMap["remote_addr"] = AnonymizeIp(parsedMap["remote_addr"])
+		generatedLogLine := ConvertMapToLogLine(parsedMap)
+		if expectedLogLine != generatedLogLine {
+			t.Errorf("Log line did not match!.\nExpected: '%s'\n      Got:'%s'", expectedLogLine, generatedLogLine)
+		}
+	})
+}
+
+// func TestLogLineString(t *testing.T) {
+// 	logLine := `52.87.65.11 - - [02/Nov/2018:06:55:13 +0000] "GET /feed.xml HTTP/1.1" 200 9810 "-" "curl"`
+// 	parsedStructure := LogLine{
+// 		remote_addr:     "52.87.65.11",
+// 		remote_user:     "",
+// 		time_local:      "02/Nov/2018:06:55:13 +0000",
+// 		request:         "GET /feed.xml HTTP/1.1",
+// 		status:          "200",
+// 		body_bytes_sent: "9810",
+// 		http_referer:    "-",
+// 		http_user_agent: "curl",
+// 	}
+// 	outputLine := LogLineString(parsedStructure)
+// 	if outputLine != logLine {
+// 		t.Errorf("expected:\n%v\n got:\n%v", logLine, outputLine)
+// 	}
+
+// }
